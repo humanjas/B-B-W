3164cc50f69fa70a4a8961c9dc84f2ea688c37e5
Jasper Hu <yj.hu@ziggo.nl>
2017-05-11 10:33:22 +0200

    Finished Block class

--- MyApplication/app/src/main/java/com/example/jasper/app/Block.java
+++ MyApplication/app/src/main/java/com/example/jasper/app/Block.java
@@ -5,10 +5,56 @@ package com.example.jasper.app;
  */
 
 public class Block {
-    public Block(String a, int b, String c, String d, boolean e) { }
-    public String getOwner() {return "a";}
-    public int getSeqNo() {return 1;}
-    public String getPrevHash() {return "a";}
-    public String getPublicKey() {return "a";}
-    public boolean isRevoked() {return false;}
+
+    private String owner, previous_hash, public_key;
+    private boolean isRevoked;
+    private int sequence_number;
+
+    public Block(String _owner, String _previous_hash, String _public_key, boolean _isRevoked, int _sequence_number) {
+        this.owner = _owner;
+        this.previous_hash = _previous_hash;
+        this.public_key = _public_key;
+        this.isRevoked = _isRevoked;
+        this.sequence_number = _sequence_number;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String _owner) {
+        this.owner = _owner;
+    }
+
+    public String getPrevious_hash() {
+        return previous_hash;
+    }
+
+    public void setPrevious_hash(String _previous_hash) {
+        this.previous_hash = _previous_hash;
+    }
+
+    public String getPublic_key() {
+        return public_key;
+    }
+
+    public void setPublic_key(String _public_key) {
+        this.public_key = _public_key;
+    }
+
+    public boolean isRevoked() {
+        return isRevoked;
+    }
+
+    public void setRevoked(boolean _revoked) {
+        isRevoked = _revoked;
+    }
+
+    public int getSequence_number() {
+        return sequence_number;
+    }
+
+    public void setSequence_number(int _sequence_number) {
+        this.sequence_number = _sequence_number;
+    }
 }
