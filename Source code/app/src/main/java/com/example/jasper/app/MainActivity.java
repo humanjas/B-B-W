51204a38c6ea9a2981ad9cc675ddb3ac54ced523
ymte <ymte@jasper-pc>
2017-05-11 13:05:37 +0200

    cleanup

--- Source code/app/src/main/java/com/example/jasper/app/MainActivity.java
+++ Source code/app/src/main/java/com/example/jasper/app/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.jasper.app;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +10,17 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onGenerateKey(View view) {
+        try {
+            /*KeyPairGenerator gen = KeyPairGenerator.getInstance("RSA");
+            gen.initialize(2048);
+            KeyPair pair = gen.generateKeyPair();
+            Store.getStore().add(pair);*/
+            Store.getStore().add("a");
+        } catch (Exception e) {
+
+        }
     }
 }
