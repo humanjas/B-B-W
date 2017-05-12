082d2ab61e9708770114b5c753978645ffa5cc51
ashay123 <a.s.s.somai@student.tudelft.nl>
2017-05-12 10:40:46 +0200

    Restored ConversionUnitTest

--- B-B-W/app/src/test/java/nl/tudelft/b_b_w/ModelsUnitTest/ConversionUnitTest.java
+++ B-B-W/app/src/test/java/nl/tudelft/b_b_w/ModelsUnitTest/ConversionUnitTest.java
@@ -1,17 +1,27 @@
 package nl.tudelft.b_b_w.ModelsUnitTest;
 
 import org.junit.Test;
+import nl.tudelft.b_b_w.Models.Conversion;
 
-import static org.junit.Assert.assertEquals;
-
+import static org.junit.Assert.*;
 /**
- * Example local unit test, which will execute on the development machine (host).
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ * ConversionTest
+ * Created by Ashay on 01/05/2017.
  */
 public class ConversionUnitTest {
+
+    /**
+     * Test to check whether the hash function gives the
+     * desired output.
+     * @throws Exception Catches error when the MessageDigest
+     * gets an error.
+     */
     @Test
-    public void addition_isCorrect() throws Exception {
-        assertEquals(4, 2 + 2);
+    public void hashCheck() throws Exception {
+        Conversion co = new Conversion("key", "owner");
+        co.hashKey();
+        String check = "54058e5f4c25b4d670ee8f37f2a1fa1941d9f0d8f3442dd4605040b5268e8056";
+
+        assertEquals(check, co.getHashedPk());
     }
 }
