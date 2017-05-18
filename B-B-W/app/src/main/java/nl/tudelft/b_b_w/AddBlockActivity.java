dd67b7dc8c3d4dba4c8d6765e80b3c65ca1bbee1
nzarin <n.zarin@student.tudelft.nl>
2017-05-18 20:02:50 +0200

    start functionality for Add

--- B-B-W/app/src/main/java/nl/tudelft/b_b_w/AddBlockActivity.java
+++ B-B-W/app/src/main/java/nl/tudelft/b_b_w/AddBlockActivity.java
@@ -2,6 +2,8 @@ package nl.tudelft.b_b_w;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+
 
 public class AddBlockActivity extends Activity {
 
@@ -9,5 +11,9 @@ public class AddBlockActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_addblock);
+    }
+
+    public void addBlock(View view){
+        
     }
 }
