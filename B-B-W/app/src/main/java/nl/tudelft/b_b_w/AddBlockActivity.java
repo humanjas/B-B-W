6c12564bf5bf66491bf4b1d98f4fb99da0b412bd
nzarin <n.zarin@student.tudelft.nl>
2017-05-18 15:54:33 +0200

    Add fields and buttons for the program: Add, Delete and show Display

---
+++ B-B-W/app/src/main/java/nl/tudelft/b_b_w/AddBlockActivity.java
@@ -0,0 +1,14 @@
+package nl.tudelft.b_b_w;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.app.Activity;
+
+public class AddBlockActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
---
+++ B-B-W/app/src/main/java/nl/tudelft/b_b_w/DeleteBlockActivity.java
@@ -0,0 +1,15 @@
+package nl.tudelft.b_b_w;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.app.Activity;
+
+
+public class DeleteBlockActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_delete);
+    }
+}
