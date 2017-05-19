4f0763182091c6c668abbcf50cdf9a018294e8b5
ymte <ymte@pc>
2017-05-18 18:43:11 -0700

    add block activity done

--- B-B-W/app/src/main/java/nl/tudelft/b_b_w/AddBlockActivity.java
+++ B-B-W/app/src/main/java/nl/tudelft/b_b_w/AddBlockActivity.java
@@ -3,17 +3,51 @@ package nl.tudelft.b_b_w;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import nl.tudelft.b_b_w.Models.Block;
+import nl.tudelft.b_b_w.Models.DatabaseHandler;
 
+/**
+ * When the user wants to add a block he enters into the AddBlockActivity, which contain
+ * some entry fields and a button to confirm the addition.
+ */
 public class AddBlockActivity extends Activity {
+    /**
+     * Connection with block database
+     */
+    private DatabaseHandler handler;
 
+    /**
+     * On create we request a database connection
+     * @param savedInstanceState unused, meant for serialisation
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_addblock);
+        handler = new DatabaseHandler(this);
     }
 
-    public void addBlock(View view){
-        
+    /**
+     * Upon adding a block, information is extracted from the GUI and put into the fresh block,
+     * which is added to the database.
+     * @param view current view (AddBlockActivity)
+     */
+    public void addBlock(View view) {
+        // extract information
+        EditText ownerText = (EditText) findViewById(R.id.addContactName);
+        EditText publicKeyText = (EditText) findViewById(R.id.addPublicKey);
+        String owner = ownerText.getText().toString();
+        String publicKey = publicKeyText.getText().toString();
+
+        // create and add the block
+        Block previous = handler.getLatestBlock(owner, publicKey);
+        Block block = new Block(owner, handler.getLatestSeqNum(owner, publicKey), "temp", previous.getOwnHash(), previous.getPreviousHashSender(), publicKey, false);
+        handler.addBlock(block);
+
+        // confirm by showing a small text message
+        Toast.makeText(this, "Block added", Toast.LENGTH_SHORT).show();
     }
 }
--- B-B-W/app/src/main/java/nl/tudelft/b_b_w/MainActivity.java
+++ B-B-W/app/src/main/java/nl/tudelft/b_b_w/MainActivity.java
@@ -5,7 +5,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import nl.tudelft.b_b_w.Models.DatabaseHandler;
+
 public class MainActivity extends Activity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
