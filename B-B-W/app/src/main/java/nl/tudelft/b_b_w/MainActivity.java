a90e0554c0f2e375b33c0a75a98334ebc9180c37
ymte <ymte@pc>
2017-05-18 19:07:25 -0700

    partial genesis fix

--- B-B-W/app/src/main/java/nl/tudelft/b_b_w/MainActivity.java
+++ B-B-W/app/src/main/java/nl/tudelft/b_b_w/MainActivity.java
@@ -8,11 +8,21 @@ import android.view.View;
 import nl.tudelft.b_b_w.Models.DatabaseHandler;
 
 public class MainActivity extends Activity {
+    private DatabaseHandler handler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        handler = new DatabaseHandler(this);
+        checkGenesis();
+    }
+
+    private void checkGenesis() {
+        if (handler.getAllBlocks().isEmpty()) {
+            // add genesis
+            // TODO create genesis block and add to database
+        }
     }
 
     public void addBlock(View view) {
