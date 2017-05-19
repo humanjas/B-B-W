8bf8982f851d96d5263a8a079648c54103d3f352
ymte <ymte@pc>
2017-05-18 17:27:59 -0700

    first callback

--- B-B-W/app/src/main/java/nl/tudelft/b_b_w/MainActivity.java
+++ B-B-W/app/src/main/java/nl/tudelft/b_b_w/MainActivity.java
@@ -1,7 +1,9 @@
 package nl.tudelft.b_b_w;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends Activity {
     public static MainActivity self;
@@ -11,5 +13,10 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         self = this;
         setContentView(R.layout.activity_main);
+    }
+
+    public void addBlock(View view) {
+        Intent intent = new Intent(this, AddBlockActivity.class);
+        startActivity(intent);
     }
 }
