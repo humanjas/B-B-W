e381be163ce41ded51f241dcb6016f31ea68d040
nzarin <n.zarin@student.tudelft.nl>
2017-05-22 15:47:11 +0200

    Gui for FriendsPage added. Functionality not complete. Test not possible, so I am writing a document.

---
+++ app/src/main/java/nl/tudelft/b_b_w/view/FriendsPageActivity.java
@@ -0,0 +1,16 @@
+package nl.tudelft.b_b_w.view;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+import nl.tudelft.b_b_w.R;
+
+public class FriendsPageActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_friends_page);
+    }
+
+}
