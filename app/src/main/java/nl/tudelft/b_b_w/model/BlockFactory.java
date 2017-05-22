6740188d8b56cc0dca4bbd5da64c156436f61f0e
MSI\NOSLEEP <T.L.Nguyen@student.tudelft.nl>
2017-05-22 17:33:23 +0200

    -Removed get sequence number from BlockFactory. -Use BlockFactory to make block in DatabaseHandler.

--- app/src/main/java/nl/tudelft/b_b_w/model/BlockFactory.java
+++ app/src/main/java/nl/tudelft/b_b_w/model/BlockFactory.java
@@ -17,7 +17,6 @@ public class BlockFactory {
      *
      * @param type                type of block
      * @param _owner              owner of block
-     * @param _sequenceNumber     sequenceNumber of block
      * @param _ownHash            Hash value of the block
      * @param _previousHashChain  Previous hash value of the block in the blockchain
      * @param _previousHashSender Previous hash value of the sender of the block
@@ -26,12 +25,12 @@ public class BlockFactory {
      * @return Returns a new block
      * @throws IllegalArgumentException when the input string type is invalid
      */
-    public static Block getBlock(String type, String _owner, int _sequenceNumber, String _ownHash, String _previousHashChain, String _previousHashSender, String _publicKey, String _iban) throws IllegalArgumentException {
+    public static Block getBlock(String type, String _owner, String _ownHash, String _previousHashChain, String _previousHashSender, String _publicKey, String _iban) throws IllegalArgumentException {
         switch (type) {
             case "BLOCK":
-                return new Block(_owner, _sequenceNumber, _ownHash, _previousHashChain, _previousHashSender, _publicKey, _iban, !TYPE_REVOKE);
+                return new Block(_owner, _ownHash, _previousHashChain, _previousHashSender, _publicKey, _iban, !TYPE_REVOKE);
             case "REVOKE":
-                return new Block(_owner, _sequenceNumber, _ownHash, _previousHashChain, _previousHashSender, _publicKey, _iban, TYPE_REVOKE);
+                return new Block(_owner, _ownHash, _previousHashChain, _previousHashSender, _publicKey, _iban, TYPE_REVOKE);
             default:
                 throw new IllegalArgumentException("Invalid type of block: " + type);
         }
