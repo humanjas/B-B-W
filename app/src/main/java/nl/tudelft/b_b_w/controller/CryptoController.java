282357b94cab55680a14882566d8217a930d76df
Jasper Hu <yj.hu@ziggo.nl>
2017-05-23 09:10:58 +0200

    Added encryption, decryption of strings and updated package of userunittest

---
+++ app/src/main/java/nl/tudelft/b_b_w/controller/CryptoController.java
@@ -0,0 +1,58 @@
+package nl.tudelft.b_b_w.controller;
+
+
+import org.encryptor4j.util.TextEncryptor;
+
+import java.security.GeneralSecurityException;
+
+/**
+ * Created by jasper on 22/05/2017.
+ */
+
+public class CryptoController {
+
+    /**
+     * Class attributes
+     * @param SECRET_KEY The key of the encryption
+     * @param textEncryptor The encryptor which uses AES Encryption in CBC mode with a maximum
+     *                      permitted key length of 256bit.
+     */
+    private final String SECRET_KEY = "k7JKPUDoGhTSo4by";
+    private TextEncryptor textEncryptor;
+
+    /**
+     * Constructor method
+     * Initializes the CryptoController by initializing the textEncryptor with the secret key
+     */
+    public CryptoController() {
+        this.textEncryptor = new TextEncryptor(SECRET_KEY);
+    }
+
+    /**
+     * encryptString method
+     * @param data given data
+     * @return encryption of data
+     * @throws GeneralSecurityException if string is larger than 256 bit
+     */
+    public String encryptString(String data) {
+        try {
+            return textEncryptor.encrypt(data);
+        }catch (GeneralSecurityException e){
+            throw new RuntimeException("Error - Could not encode string: " + e);
+        }
+    }
+
+    /**
+     * decryptString method
+     * @param data given encrypted data
+     * @return decrypted data
+     * @throws GeneralSecurityException if string is larger than 256 bit
+     */
+    public String decryptString(String data) {
+        try {
+            return textEncryptor.decrypt(data);
+        }catch (GeneralSecurityException e){
+            throw new RuntimeException("Error - Could not decode string: " + e);
+        }
+    }
+}
