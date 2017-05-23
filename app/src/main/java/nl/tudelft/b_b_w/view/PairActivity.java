4abfc7ee712e45841df5cde369d7d72fc16e74d3
nzarin <n.zarin@student.tudelft.nl>
2017-05-23 23:36:08 +0200

    Linked the pair activity to test subject files

--- app/src/main/java/nl/tudelft/b_b_w/view/PairActivity.java
+++ app/src/main/java/nl/tudelft/b_b_w/view/PairActivity.java
@@ -1,9 +1,12 @@
 package nl.tudelft.b_b_w.view;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
 
 import nl.tudelft.b_b_w.R;
+import nl.tudelft.b_b_w.view.TestSubjects.TestSubject1;
 
 public class PairActivity extends Activity {
 
@@ -11,6 +14,23 @@ public class PairActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pair);
+    }
+
+
+    public void onTestSubject1(View view) {
+        Intent intent = new Intent(this, TestSubject1.class);
+        startActivity(intent);
+    }
+
+    public void onTestSubject2(View view) {
+        Intent intent = new Intent(this, TestSubject1.class);
+        startActivity(intent);
+    }
+
+
+    public void onTestSubject3(View view) {
+        Intent intent = new Intent(this, TestSubject1.class);
+        startActivity(intent);
     }
 
 }
