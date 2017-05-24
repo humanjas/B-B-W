f5360f35d20ab218d69c80dcc1dade9354f49f57
ashay123 <a.s.s.somai@student.tudelft.nl>
2017-05-24 17:31:32 +0200

    -Changed the structure of displaying a block from your chain on the Contacts page. -After each name and IBAN, there is also a revoke button. -Added functionality for revoke button. -After revoking, the screen is automatically refreshed, removing revoked blocks from the display. -When you press revoke, you'll get a pop-up to confirm the revoke.

--- app/src/main/java/nl/tudelft/b_b_w/view/ContactsActivity.java
+++ app/src/main/java/nl/tudelft/b_b_w/view/ContactsActivity.java
@@ -1,56 +1,99 @@
 package nl.tudelft.b_b_w.view;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.ListAdapter;
 import android.widget.ListView;
-import android.widget.Toast;
-
-import java.util.List;
+import android.widget.TextView;
 
 import nl.tudelft.b_b_w.R;
 import nl.tudelft.b_b_w.controller.BlockController;
-import nl.tudelft.b_b_w.model.Block;
-
-import static nl.tudelft.b_b_w.R.id.contacts;
 
 /**
  * When the user wants to add a block he enters into the ContactsActivity, which contain
  * some entry fields and a button to confirm the addition.
  */
 public class ContactsActivity extends Activity {
-    public class ContactAdapter extends BaseAdapter {
-        private LayoutInflater inflater;
-        private List<Block> blocks;
+    public class ContactAdapter extends BaseAdapter implements ListAdapter {
+        private BlockController bc;
+        Context context;
 
-        public ContactAdapter(List<Block> blocks) {
-            super();
-            inflater = (LayoutInflater)getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            this.blocks = blocks;
+        public ContactAdapter(BlockController bc, Context context) {
+            this.context = context;
+            this.bc = bc;
         }
 
         @Override
-        public String getItem(int position) {
-            return blocks.get(position).toString();
+        public Object getItem(int position) {
+            return bc.getBlocks("GENESIS").get(position);
         }
 
         @Override
         public long getItemId(int position) {
-            return position;
+            return 0;
         }
 
         @Override
         public int getCount() {
-            return blocks.size();
+            return bc.getBlocks("GENESIS").size();
         }
 
         @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
-            return inflater.inflate(R.layout.simple_list_item_1, null);
+        public View getView(final int position, View convertView, ViewGroup parent) {
+            View view = convertView;
+            if (view == null) {
+                LayoutInflater inflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                view = inflater.inflate(R.layout.simple_list_item_1, null);
+            }
+
+            TextView nameItemText = (TextView)view.findViewById(R.id.list_item_name);
+            nameItemText.setText(bc.getBlocks("GENESIS").get(position).getOwner()); //use function to backtrack owner
+            TextView ibanItemText = (TextView)view.findViewById(R.id.list_item_iban);
+            ibanItemText.setText(bc.getBlocks("GENESIS").get(position).getPublicKey()); //change to IBAN
+
+            Button revokeButton = (Button)view.findViewById(R.id.revoke_btn);
+
+            revokeButton.setOnClickListener(new View.OnClickListener() {
+               @Override
+                public void onClick(View v) {
+                   AlertDialog.Builder builder = new AlertDialog.Builder(context);
+
+                   builder.setTitle("Confirm");
+                   builder.setMessage("Are you sure you want to revoke "+ bc.getBlocks("GENESIS").get(position).getOwner()+ " IBAN?");
+
+                   builder.setPositiveButton("YES", new DialogInterface.OnClickListener() {
+
+                       public void onClick(DialogInterface dialog, int which) {
+                           bc.revokeBlock(bc.getBlocks("GENESIS").get(position));
+                           notifyDataSetChanged();
+                           dialog.dismiss();
+                       }
+                   });
+
+                   builder.setNegativeButton("NO", new DialogInterface.OnClickListener() {
+
+                       @Override
+                       public void onClick(DialogInterface dialog, int which) {
+
+                           // Do nothing
+                           dialog.dismiss();
+                       }
+                   });
+
+                   AlertDialog alert = builder.create();
+                   alert.show();
+               }
+            });
+
+            return view;
         }
     }
     /**
@@ -65,16 +108,22 @@ public class ContactsActivity extends Activity {
 
         // get contacts
         BlockController bc = new BlockController(this);
-        List<Block> blocks = bc.getBlocks("GENESIS");
+//        bc.addBlock(BlockFactory.getBlock("BLOCK", "GENESIS", 2,
+//                "Hash", "prevhash", "prevhashsender",
+//                "pubkey"));
 
-        try {
-            // list
-            ListView view = (ListView) findViewById(contacts);
-            ContactAdapter adapter = new ContactAdapter(blocks);
-            view.setAdapter(adapter);
-        } catch (Exception e) {
-            Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG).show();
-        }
+        ContactAdapter adapter = new ContactAdapter(bc, this);
+        ListView lView = (ListView)findViewById(R.id.contacts);
+        lView.setAdapter(adapter);
+
+//        try {
+//            // list
+//            ListView view = (ListView) findViewById(contacts);
+//            ContactAdapter adapter = new ContactAdapter(bc);
+//            view.setAdapter(adapter);
+//        } catch (Exception e) {
+//            Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG).show();
+//        }
     }
 }
 
